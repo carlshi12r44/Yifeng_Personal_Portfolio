5619f7f001af65e3207103e9347d29ed6d5d4003
yifeng Shi <yifengshi@yifengs-MBP-2.lan>
2021-02-14 22:08:29 -0800

    added animation for skill stack

--- src/components/MyStack.js
+++ src/components/MyStack.js
@@ -19,56 +19,37 @@ const MyStack = () => {
           <div className="row make-it-flex">
             <div className="col-md-2 col-sm-3">
               <div className="box">
-                <div className="circle">
+                <div className="circle-up">
                   <img className="reactLogo" src={ReactLogo} alt="React Logo" />
                 </div>
-                <h3>React</h3>
-                <p>
-                  I used React JS for my internships and most web development
-                  projects
-                </p>
               </div>
             </div>
             <div className="col-md-2 col-sm-3">
               <div className="box">
-                <div className="circle">
+                <div className="circle-down">
                   <img className="JSLogo" src={JsLogo} alt="Javascript Logo" />
                 </div>
-                <h3>Javascript</h3>
-                <p>I used Javascript to implement most React component logic</p>
               </div>
             </div>
             <div className="col-md-2 col-sm-3">
               <div className="box">
-                <div className="circle">
+                <div className="circle-up">
                   <img className="CSSLogo" src={cssLogo} alt="CSS Logo" />
                 </div>
-                <h3>CSS 3</h3>
-                <p>
-                  I used CSS 3 to style React component and pages for user
-                  experience
-                </p>
               </div>
             </div>
             <div className="col-md-2 col-sm-3">
               <div className="box">
-                <div className="circle">
+                <div className="circle-down">
                   <img className="HTMLLogo" src={htmlLogo} alt="HTML 5 Logo" />
                 </div>
-                <h3>HTML 5</h3>
-                <p>I used HTML 5 to construct page structures and fundation.</p>
               </div>
             </div>
             <div className="col-md-2 col-sm-3">
               <div className="box">
-                <div className="circle">
+                <div className="circle-up">
                   <img className="MySQLLogo" src={MySQLLogo} alt="MySQL Logo" />
                 </div>
-                <h3>MySQL</h3>
-                <p>
-                  I used MySQL database to store backend routes for user
-                  information in some of my projects
-                </p>
               </div>
             </div>
           </div>
